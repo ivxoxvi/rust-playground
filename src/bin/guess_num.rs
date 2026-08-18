6e7f2692310e1e50ec1f7d92cab4c51d357a1e60
vxoxv <lilivxoxvilil@gmail.com>
2026-08-18 22:19:27 +0800

    init

---
+++ src/bin/guess_num.rs
@@ -0,0 +1,33 @@
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("you guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Equal => {
+                println!("success");
+                break;
+            }
+            Ordering::Greater => println!("too big"),
+        }
+    }
+}
